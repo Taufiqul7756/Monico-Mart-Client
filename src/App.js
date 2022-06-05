272bf9b7c6858ee99135851ececf3d9614c2191c
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2022-06-06 01:12:14 +0600

    Added React Router v5.1.2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./Components/Header/Header";
+import { createContext, useState } from "react";
+import NoMatch from "./Components/NoMatch/NoMatch";
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Switch,
+  Link,
+} from "react-router-dom";
+import Shop from "./Components/Shop/Shop";
+
+export const UserContext = createContext();
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header></Header>
+        <Switch>
+          <Route path="/shop">
+            <Shop></Shop>
+          </Route>
+
+          <Route exact path="/">
+            <Shop></Shop>
+          </Route>
+          {/* <Route path="*">
+            <NoMatch></NoMatch>
+          </Route> */}
+        </Switch>
+      </Router>
     </div>
   );
 }
