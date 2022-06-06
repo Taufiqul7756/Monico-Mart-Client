ccc08f1e024ddf69ac1d7f980137026a67331667
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2022-06-06 23:55:42 +0600

    Profile updated ( phot , name and gmail show)

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -1,7 +1,19 @@
 import React from "react";
+import "./Profile.css";
 
-const Profile = () => {
-  return <div></div>;
+const Profile = ({ user }) => {
+  console.log(user);
+  return (
+    <div className="profile-container">
+      <div className="photo-container">
+        <img src={user.photo} alt="" />
+      </div>
+      <div className="details-container">
+        <h3>Name: {user.name}</h3>
+        <h3>Email: {user.email}</h3>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
