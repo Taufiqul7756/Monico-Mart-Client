f1deb7b357bc0faef6c045e86f32afed153b365c
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2022-06-07 15:27:07 +0600

    added logo of Monico Tech ltd

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useContext } from "react";
 import { UserContext } from "../../App";
 import { Link } from "react-router-dom";
+import logo from "../../images/logo1.png";
 import "./Header.css";
 
 const Header = ({ signOut }) => {
@@ -9,7 +10,7 @@ const Header = ({ signOut }) => {
 
   return (
     <div className="header">
-      {/* <img src={logo} alt="" /> */}
+      <img src={logo} alt="" />
       <nav>
         <Link to="/shop">Shop</Link>
         <Link to="/review">Review Cart</Link>
