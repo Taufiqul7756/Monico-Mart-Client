ee2aa97be658329857d5a8e3eab18b20afdd32c3
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2022-06-07 12:39:59 +0600

    Updated but not using yet

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,7 +4,7 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <div className="footer-container">
-      <h1>This is Footer Section</h1>
+      <h1 className="details-container">This is Footer Section</h1>
     </div>
   );
 };
