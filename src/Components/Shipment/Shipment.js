2946b4bdf0106e81b2f27e6dbdccc4f8f078c1ee
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2022-06-07 12:44:07 +0600

    Success photo added and FormSubmitted button change

--- src/Components/Shipment/Shipment.js
+++ src/Components/Shipment/Shipment.js
@@ -1,50 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import "./Shipment.css";
 import { useContext } from "react";
 import { UserContext } from "../../App";
+import HappyPhoto from "../../images/giphy.gif";
+import { Link } from "react-router-dom";
 
 const Shipment = () => {
-  const { register, handleSubmit, errors } = useForm();
+  const { register, handleSubmit, watch, errors } = useForm();
+  const [formSubmitted, setFormSubmitted] = useState(false);
   const [loggedInUser, setLoggedInUser] = useContext(UserContext);
-  const onSubmit = (data) => {
-    console.log("form submitted", data);
-  };
+  // const onSubmit = (data) => {
+  //   console.log("form submitted", data);
+  // };
+  //onSubmit={handleSubmit(onSubmit)}
 
   return (
-    <form className="ship-form" onSubmit={handleSubmit(onSubmit)}>
-      <input
-        name="name"
-        defaultValue={loggedInUser.name}
-        ref={register({ required: true })}
-        placeholder="Your Name"
-      />
-      {errors.name && <span className="error">Name is required</span>}
-
-      <input
-        name="email"
-        defaultValue={loggedInUser.email}
-        ref={register({ required: true })}
-        placeholder="Your Email"
-      />
-      {errors.email && <span className="error">Email is required</span>}
-
-      <input
-        name="address"
-        ref={register({ required: true })}
-        placeholder="Your Address"
-      />
-      {errors.address && <span className="error">Address is required</span>}
-
-      <input
-        name="phone"
-        ref={register({ required: true })}
-        placeholder="Your Phone Number"
-      />
-      {errors.phone && <span className="error">Phone Number is required</span>}
-
-      <input type="submit" />
-    </form>
+    <>
+      {!formSubmitted ? (
+        <form className="ship-form">
+          <input
+            name="name"
+            defaultValue={loggedInUser.name}
+            ref={register({ required: true })}
+            placeholder="Your Name"
+          />
+          {errors.name && <span className="error">Name is required</span>}
+          <input
+            name="email"
+            defaultValue={loggedInUser.email}
+            ref={register({ required: true })}
+            placeholder="Your Email"
+          />
+          {errors.email && <span className="error">Email is required</span>}
+          <input
+            name="address"
+            ref={register({ required: true })}
+            placeholder="Your Address"
+          />
+          {errors.address && <span className="error">Address is required</span>}
+          <input
+            name="phone"
+            ref={register({ required: true })}
+            placeholder="Your Phone Number"
+          />
+          {errors.phone && (
+            <span className="error">Phone Number is required</span>
+          )}
+          <button
+            onClick={() => setFormSubmitted(true)}
+            className="shipment-button"
+            type="submit"
+          >
+            SUBMIT
+          </button>
+        </form>
+      ) : (
+        <img
+          className="success-gif"
+          src="https://media3.giphy.com/media/a0h7sAqON67nO/giphy.gif"
+          alt=""
+        />
+      )}
+    </>
   );
 };
 
