23aee7f801dceea3e68ae4080b5311869c2c86a0
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2022-06-07 15:23:45 +0600

    Test- Login page . Not working

---
+++ src/Components/Login/LoginTest.js
@@ -0,0 +1,304 @@
+import { useContext, useState } from "react";
+import { useHistory, useLocation } from "react-router-dom";
+import { UserContext } from "../../App";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import {
+  SignupHandler,
+  signInHandler,
+  handleFbSignIn,
+  handleGoogleSignIn,
+  handleSignOut,
+  initializeLoginForm,
+} from "./LoginManager";
+
+initializeLoginForm();
+
+function LoginTest() {
+  const [newUser, setNewUser] = useState(false);
+  const [user, setUser] = useState({
+    isSignedIn: false,
+    newUser: false,
+    name: "",
+    email: "",
+    password: "",
+    photo: "",
+  });
+
+  const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+  const history = useHistory();
+  const location = useLocation();
+  const { from } = location.state || { from: { pathname: "/" } };
+
+  const googleSignIn = () => {
+    handleGoogleSignIn().then((res) => {
+      setUser(res);
+      setLoggedInUser(res);
+      history.replace(from);
+    });
+  };
+
+  const FbSignIn = () => {
+    handleFbSignIn().then((res) => {
+      setUser(res);
+      setLoggedInUser(res);
+      history.replace(from);
+    });
+  };
+
+  const SignOut = () => {
+    handleSignOut().then((res) => {
+      setUser(res);
+      setLoggedInUser(res);
+    });
+  };
+
+  const handleBlur = (e) => {
+    let isFieldValid = true;
+    if (e.target.name === "email") {
+      isFieldValid = /\S+@\S+\.\S+/.test(e.target.value);
+      //console.log(isFormValid);
+    }
+    if (e.target.name === "password") {
+      const isPasswordValid = e.target.value.length > 6;
+      const isPasswordHasNumber = /\d{1}/.test(e.target.value);
+      isFieldValid = isPasswordValid && isPasswordHasNumber;
+      //console.log(isPasswordValid && isPasswordHasNumber);
+    }
+    if (isFieldValid) {
+      const newUserInfo = { ...user };
+      newUserInfo[e.target.name] = e.target.value;
+      setUser(newUserInfo);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    if (newUser && user.email && user.password) {
+      SignupHandler(user.name, user.email, user.password).then((res) => {
+        setUser(res);
+        setLoggedInUser(res);
+        history.replace(from);
+      });
+    }
+
+    if (!newUser && user.email && user.password) {
+      signInHandler(user.email, user.password).then((res) => {
+        setUser(res);
+        setLoggedInUser(res);
+        history.replace(from);
+      });
+    }
+    e.preventDefault();
+  };
+
+  const handleClick = () => {
+    console.log("Clicked");
+    let path = "/login";
+    history.push(path);
+  };
+
+  return (
+    <div className="Auth-container">
+      <div className="login-container">
+        <Container className="signIn-container" component="main" maxWidth="xs">
+          <CssBaseline />
+          <Box
+            sx={{
+              marginTop: 8,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+              {/* <LockOutlinedIcon /> */}
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <Box
+              component="form"
+              onSubmit={handleSubmit}
+              noValidate
+              sx={{ mt: 1 }}
+            >
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign In
+              </Button>
+              <Grid container>
+                <Grid item xs>
+                  <Link href="#" variant="body2">
+                    Forgot password?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link
+                    className="cursor"
+                    onClick={handleClick}
+                    variant="body2"
+                  >
+                    {"Don't have an account? Sign Up"}
+                  </Link>
+                </Grid>
+              </Grid>
+            </Box>
+          </Box>
+        </Container>
+      </div>
+      <div className="signUp-container">
+        <Container className="signup-container" component="main" maxWidth="xs">
+          <CssBaseline />
+          <Box
+            sx={{
+              marginTop: 8,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+              {/* <LockOutlinedIcon /> */}
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign up
+            </Typography>
+            <Box
+              component="form"
+              noValidate
+              onSubmit={handleSubmit}
+              sx={{ mt: 3 }}
+            >
+              <Grid container spacing={2}>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    autoComplete="given-name"
+                    name="firstName"
+                    required
+                    fullWidth
+                    id="firstName"
+                    label="First Name"
+                    autoFocus
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="lastName"
+                    label="Last Name"
+                    name="lastName"
+                    autoComplete="family-name"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="email"
+                    label="Email Address"
+                    name="email"
+                    autoComplete="email"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type="password"
+                    id="password"
+                    autoComplete="new-password"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <FormControlLabel
+                    control={
+                      <Checkbox value="allowExtraEmails" color="primary" />
+                    }
+                    label="I want to receive inspiration, marketing promotions and updates via email."
+                  />
+                </Grid>
+              </Grid>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign Up
+              </Button>
+              <Grid container justifyContent="flex-end">
+                <Grid item>
+                  <button
+                    onClick={handleGoogleSignIn}
+                    type="button"
+                    class="login-with-google-btn"
+                  >
+                    Sign in with Google
+                  </button>
+                  <button
+                    onClick={handleFbSignIn}
+                    type="button"
+                    class="login-with-google-btn"
+                  >
+                    Sign in with Facebook
+                  </button>
+
+                  <Link
+                    className="cursor"
+                    onClick={handleClick}
+                    variant="body2"
+                  >
+                    Already have an account? Sign in
+                  </Link>
+                </Grid>
+              </Grid>
+            </Box>
+          </Box>
+          <div></div>
+        </Container>
+      </div>
+    </div>
+  );
+}
+
+export default LoginTest;
