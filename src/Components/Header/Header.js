7654235bde804ae579f9ef9ed7cf13db778bad84
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2022-06-07 12:37:15 +0600

    sign in and Log out button updated

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useContext } from "react";
 import { UserContext } from "../../App";
 import { Link } from "react-router-dom";
@@ -6,24 +6,26 @@ import "./Header.css";
 
 const Header = ({ signOut }) => {
   const [loggedInUser, setloggedInUser] = useContext(UserContext);
+
   return (
     <div className="header">
       {/* <img src={logo} alt="" /> */}
       <nav>
         <Link to="/shop">Shop</Link>
         <Link to="/review">Review Cart</Link>
-        <Link to="/profile">
-          {!loggedInUser ? "Profile" : loggedInUser.name}
+        {loggedInUser.isSignedIn && (
+          <Link to="/profile">
+            {loggedInUser.name}
 
-          {/* {loggedInUser && loggedInUser.name} {loggedInUser && "Profile"} */}
+            {/* {loggedInUser && loggedInUser.name} {loggedInUser && "Profile"} */}
+          </Link>
+        )}
+
+        <Link to={!loggedInUser.isSignedIn && "/login"}>
+          <span onClick={() => setloggedInUser({})}>
+            {!loggedInUser.isSignedIn ? "SignIn" : "Sign Out"}
+          </span>
         </Link>
-        <button
-          onClick={() => {
-            setloggedInUser({});
-          }}
-        >
-          Sign Out
-        </button>
       </nav>
     </div>
   );
