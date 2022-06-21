3a92b03f62f184cd3e83b15c8a0a919dedf18911
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2022-06-21 23:42:57 +0600

    Firebase config CDN

---
+++ src/Components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA2DQfUKogok3KBMUQUnmTIo656woqGYVM",
+  authDomain: "monicotech-auth.firebaseapp.com",
+  projectId: "monicotech-auth",
+  storageBucket: "monicotech-auth.appspot.com",
+  messagingSenderId: "802567550163",
+  appId: "1:802567550163:web:f865d0a0ef2a625d06d026",
+};
+export default firebaseConfig;
